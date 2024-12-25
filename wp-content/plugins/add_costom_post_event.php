3304979934c9b1271791bdb5dce57cf0076301fd
NhatNguyen4994 <nguyenducnhat4994@gmail.com>
2024-12-25 12:45:57 +0700

    update

---
+++ wp-content/plugins/add_costom_post_event.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: add custom post tyoe event
+Plugin URI: http://wordpress.org/plugin
+Description:add custom post type event 
+Author: ndnhat
+Version: 1
+Author URI: http://ma.tt/
+*/
+
+
